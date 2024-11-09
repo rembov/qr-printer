da83045dd4801e4257ab881f3ce33198aec2a2a6
rembov <i7888@mail.ru>
2024-11-09 22:13:42 +0300

    exe

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import configparser
 
 CONFIG_FILE = "config.ini"
 
+
 class QRPrinterApp:
     def __init__(self, root):
         self.root = root
@@ -41,6 +42,7 @@ class QRPrinterApp:
             self.selected_printer.set(self.printers[0] if self.printers else "")
         self.printer_combo.bind("<<ComboboxSelected>>", self.save_printer)
         self.generated_image = None
+
     def add_context_menu(self, widget):
 
         context_menu = tk.Menu(widget, tearoff=0)
@@ -53,16 +55,19 @@ class QRPrinterApp:
         widget.bind("<Control-c>", lambda event: widget.event_generate("<<Copy>>"))
         widget.bind("<Control-a>", lambda event: widget.event_generate("<<SelectAll>>"))
         widget.bind("<Control-z>", lambda event: widget.event_generate("<<Undo>>"))
+
     def load_config(self):
         if os.path.exists(CONFIG_FILE):
             self.config.read(CONFIG_FILE)
         else:
             self.config["Settings"] = {}
+
     def save_printer(self, event=None):
         printer_name = self.selected_printer.get()
         self.config["Settings"]["printer"] = printer_name
         with open(CONFIG_FILE, "w") as config_file:
             self.config.write(config_file)
+
     def download_image(self):
         url = self.url_entry.get().strip()
         if not url:
@@ -78,11 +83,13 @@ class QRPrinterApp:
             self.show_image()
         except Exception as e:
             messagebox.showerror("Ошибка", f"Не удалось загрузить изображение: {str(e)}")
+
     def show_image(self):
         img = self.generated_image.resize((200, 200), Image.Resampling.LANCZOS)
         img_tk = ImageTk.PhotoImage(img)
         self.image_label.config(image=img_tk)
         self.image_label.image = img_tk
+
     def print_code(self):
         if not self.generated_image:
             messagebox.showerror("Ошибка", "Сначала загрузите изображение!")
@@ -105,6 +112,8 @@ class QRPrinterApp:
         hdc.DeleteDC()
         os.remove(temp_path)
         messagebox.showinfo("Успех", "Изображение успешно отправлено на печать!")
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     app = QRPrinterApp(root)
